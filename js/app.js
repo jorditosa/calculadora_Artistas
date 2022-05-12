ef9167527a846fc7c426abdfb5a623b425c95369
jorditosa <73916178+jorditosa@users.noreply.github.com>
2022-05-12 13:49:24 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,104 @@
+//preventDefault
+const submit = document.querySelector('#formulario');
+
+submit.addEventListener('submit', (e) => {
+    e.preventDefault();
+})
+
+// Obtencion de datos
+   
+let salarioIn = document.querySelector('#salario');
+let salary;
+let diasIn = document.querySelector('#dias');
+let days;
+let irpfIn = document.querySelector('#irpf');
+let irpf;
+
+salarioIn.addEventListener('input', (e) => {
+    salary = parseFloat(e.target.value);
+    console.log(salary);
+})
+diasIn.addEventListener('input', (e) => {
+    days = parseInt(e.target.value);
+    console.log(days);
+})
+irpfIn.addEventListener('input', (e) => {
+    irpf = (parseFloat(e.target.value)/100);
+    console.log(irpf);
+})
+
+// calculadora
+const formulario = document.querySelector('#formulario');
+formulario.addEventListener('submit', calculadora);
+
+
+
+function calculadora(){
+
+    let salaryDaily = parseFloat(salary / days);
+    console.log(salaryDaily);
+    let segsocCost;
+    let segsocWorker;
+
+    try {
+        if( isNaN(salary) === true|| isNaN(days) === true || isNaN(irpf) === true) {
+            throw error;
+        }
+        
+        if ( salaryDaily <= 275.00 ) {
+            segsocCost = salaryDaily * days * 0.326;
+        } else if (salaryDaily > 275.00 && salaryDaily <= 469.00){
+            segsocCost = 275 * days * 0.326;
+        } else if (salaryDaily > 469.00 && salaryDaily <= 843.00) {
+            segsocCost = 347 * days * 0.326;
+        } else if (salaryDaily > 843.00 && salaryDaily <= 1410.00) {
+            segsocCost = 414 * days * 0.326;
+        } else {
+            segsocCost = 551 * days * 0.326;
+        }
+        
+        // Cálculo NETO
+        if ( salaryDaily <= 275.00 ) {
+            segsocWorker = salaryDaily * days * 0.064;
+        } else if (salaryDaily > 275.00 && salaryDaily <= 469.00){
+            segsocWorker = 275 * days * 0.064;
+        } else if (salaryDaily > 469.00 && salaryDaily <= 843.00) {
+            segsocWorker = 347 * days * 0.064;
+        } else if (salaryDaily > 843.00 && salaryDaily <= 1410.00) {
+            segsocWorker = 414 * days * 0.064;
+        } else {
+            segsocWorker = 551 * days * 0.064;
+        }
+    } catch (error) {
+        alert('Por favor, introduce datos válidos');
+    }
+
+    // Control bases máximas y mínimas
+
+    if(segsocCost > 1349.40) {
+        segsocCost = 1349.40;
+    }
+    if(segsocWorker > 264.92) {
+        segsocWorker = 264.92;
+    }
+    
+    // Calculos e imprimir resultado por pantalla
+    let totalCost = salary + segsocCost;
+    let totalCostText = document.querySelector('.calculadora__coste--text');
+    let costResult = document.querySelector('.calculadora__coste');
+
+    totalCostText.innerHTML = 'El coste total de la nómina será de: '
+    costResult.innerHTML =  totalCost.toFixed(2)+'€.';
+    
+
+    parseFloat(irpfGross = salary * irpf);
+    let net = salary - segsocWorker - irpfGross;
+    let totalResultText = document.querySelector('.calculadora__neto--text');
+    let netResult = document.querySelector('.calculadora__neto');
+
+    netResult.innerHTML = net.toFixed(2) +'€.';
+    totalResultText.innerHTML = 'El neto a percibir será de: '
+
+};
+
+
---
+++ js/interactive.js
@@ -0,0 +1,54 @@
+// Variabkes
+const btnEnviar = document.querySelector('#formCalc');
+const formulario = document.querySelector('#formulario');
+
+let salarioIn = document.querySelector('#salario');
+let diaIn = document.querySelector('#dias');
+let irpfIn = document.querySelector('#irpf');
+
+eventListeners();
+
+function eventListeners(){
+    // Cuando la app arranca
+    document.addEventListener('DOMContentLoaded', iniciarApp);
+
+    // Campos del formulario
+    salarioIn.addEventListener('blur', validarformulario)
+    diaIn.addEventListener('blur', validarformulario)
+    irpfIn.addEventListener('blur', validarformulario)
+}
+
+
+// funciones
+function iniciarApp() {
+    btnEnviar.disabled = true;
+    btnEnviar.classList.add('opacity-50')
+}
+
+// valida el formulario
+
+function validarformulario(e){
+
+    if(e.target.value.length > 0) {
+
+    } else {
+        e.target.style.borderColor = 'red';
+
+        mostrarError();
+    }
+}
+
+function mostrarError(){
+    let mensajeError = document.createElement('p');
+    mensajeError.textContent = 'Todos los campos son obligatorios';
+    mensajeError.classList.add('error-text');
+
+    const errores = document.querySelectorAll('.error-text');
+    if(errores.length === 0){
+        formulario.appendChild(mensajeError);
+    } else {
+
+    }
+
+    
+}
