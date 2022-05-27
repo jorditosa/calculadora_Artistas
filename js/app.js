a7ca6588140ad41d5f500efa3f973c72e29f1ada
jorditosa <73916178+jorditosa@users.noreply.github.com>
2022-05-27 17:28:15 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,10 +1,3 @@
-//preventDefault
-const submit = document.querySelector('#formulario');
-
-submit.addEventListener('submit', (e) => {
-    e.preventDefault();
-})
-
 // Obtencion de datos
    
 let salarioIn = document.querySelector('#salario');
@@ -13,7 +6,9 @@ let diasIn = document.querySelector('#dias');
 let days;
 let irpfIn = document.querySelector('#irpf');
 let irpf;
+const formulario = document.querySelector('#formulario');
 
+// EVENT LISTENERS
 salarioIn.addEventListener('input', (e) => {
     salary = parseFloat(e.target.value);
     console.log(salary);
@@ -26,14 +21,12 @@ irpfIn.addEventListener('input', (e) => {
     irpf = (parseFloat(e.target.value)/100);
     console.log(irpf);
 })
-
-// calculadora
-const formulario = document.querySelector('#formulario');
 formulario.addEventListener('submit', calculadora);
 
 
-
-function calculadora(){
+// FUNCIONES
+function calculadora(e){
+    e.preventDefault();
 
     let salaryDaily = parseFloat(salary / days);
     console.log(salaryDaily);
@@ -44,33 +37,69 @@ function calculadora(){
         if( isNaN(salary) === true|| isNaN(days) === true || isNaN(irpf) === true) {
             throw error;
         }
-        
-        if ( salaryDaily <= 275.00 ) {
-            segsocCost = salaryDaily * days * 0.326;
-        } else if (salaryDaily > 275.00 && salaryDaily <= 469.00){
-            segsocCost = 275 * days * 0.326;
-        } else if (salaryDaily > 469.00 && salaryDaily <= 843.00) {
-            segsocCost = 347 * days * 0.326;
-        } else if (salaryDaily > 843.00 && salaryDaily <= 1410.00) {
-            segsocCost = 414 * days * 0.326;
-        } else {
-            segsocCost = 551 * days * 0.326;
+        // Cálculo COSTE SEG SOCIAL
+        switch (true){
+            case ( salaryDaily <= 275.00 ):
+                segsocCost = salaryDaily * days * 0.326;
+            break;
+            case (salaryDaily > 275.00 && salaryDaily <= 469.00):
+                segsocCost = 275 * days * 0.326;
+            break;
+            case (salaryDaily > 469.00 && salaryDaily <= 843.00): 
+                segsocCost = 347 * days * 0.326;
+            break;
+            case (salaryDaily > 843.00 && salaryDaily <= 1410.00): 
+                segsocCost = 414 * days * 0.326;
+            break;
+            default:
+                segsocCost = 551 * days * 0.326;
         }
-        
+            
         // Cálculo NETO
-        if ( salaryDaily <= 275.00 ) {
-            segsocWorker = salaryDaily * days * 0.064;
-        } else if (salaryDaily > 275.00 && salaryDaily <= 469.00){
-            segsocWorker = 275 * days * 0.064;
-        } else if (salaryDaily > 469.00 && salaryDaily <= 843.00) {
-            segsocWorker = 347 * days * 0.064;
-        } else if (salaryDaily > 843.00 && salaryDaily <= 1410.00) {
-            segsocWorker = 414 * days * 0.064;
-        } else {
-            segsocWorker = 551 * days * 0.064;
+        switch (true){
+            case ( salaryDaily <= 275.00 ):
+                segsocWorker = salaryDaily * days * 0.064;
+            break;
+            case (salaryDaily > 275.00 && salaryDaily <= 469.00):
+                segsocWorker = 275 * days * 0.064;
+            break;
+            case (salaryDaily > 469.00 && salaryDaily <= 843.00):
+                segsocWorker = 347 * days * 0.064;
+            break;
+            case (salaryDaily > 843.00 && salaryDaily <= 1410.00): 
+                segsocWorker = 414 * days * 0.064;
+            default:
+                segsocWorker = 551 * days * 0.064;
         }
+
+        // Calculos e imprimir resultado por pantalla
+        let totalCost = salary + segsocCost;
+        let totalCostText = document.querySelector('.calculadora__coste--text');
+        let costResult = document.querySelector('.calculadora__coste');
+
+        totalCostText.innerHTML = 'El coste total de la nómina será de: '
+        costResult.innerHTML =  (new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(totalCost));
+        
+
+        parseFloat(irpfGross = salary * irpf);
+        let net = salary - segsocWorker - irpfGross;
+        let totalResultText = document.querySelector('.calculadora__neto--text');
+        let netResult = document.querySelector('.calculadora__neto');
+
+        netResult.innerHTML = (new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(net));
+        totalResultText.innerHTML = 'El neto a percibir será de: '
+
     } catch (error) {
-        alert('Por favor, introduce datos válidos');
+        let errorBox = document.createElement('p');
+        errorBox.textContent = "Por favor, introduce datos válidos";
+        errorBox.classList.add('error');
+
+        let errorAlert = document.querySelector('.calculadora__results');
+        errorAlert.appendChild(errorBox);
+
+        setTimeout(() => {
+            errorAlert.remove();
+        }, 4000)
     }
 
     // Control bases máximas y mínimas
@@ -84,25 +113,6 @@ function calculadora(){
     if(salaryDaily < 39){
         salaryDaily = 39;
     }
-    
-    // Calculos e imprimir resultado por pantalla
-    let totalCost = salary + segsocCost;
-    let totalCostText = document.querySelector('.calculadora__coste--text');
-    let costResult = document.querySelector('.calculadora__coste');
-
-    totalCostText.innerHTML = 'El coste total de la nómina será de: '
-    costResult.innerHTML =  (new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(totalCost));
-    
-
-    parseFloat(irpfGross = salary * irpf);
-    let net = salary - segsocWorker - irpfGross;
-    let totalResultText = document.querySelector('.calculadora__neto--text');
-    let netResult = document.querySelector('.calculadora__neto');
-
-    netResult.innerHTML = (new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(net));
-    totalResultText.innerHTML = 'El neto a percibir será de: '
-
- 
 };
 
 
