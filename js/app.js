2b29f2280128d16cbae2a0c0e77ce7b3d18a03bc
jorditosa <73916178+jorditosa@users.noreply.github.com>
2022-07-14 00:26:48 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // Obtencion de datos
-   
+
 let salarioIn = document.querySelector('#salario');
 let salary;
 let diasIn = document.querySelector('#dias');
@@ -10,132 +10,143 @@ const formulario = document.querySelector('#formulario');
 
 const resultAlert = document.querySelector('.calculadora__results');
 const resultClose = document.querySelector('.result-overlay_close');
-const resultError = document.querySelector('#formCalc');
+const resultBtn = document.querySelector('#formCalc');
 
-// EVENT LISTENERS
-salarioIn.addEventListener('input', (e) => {
-    salary = parseFloat(e.target.value);
-    console.log(salary);
-})
-diasIn.addEventListener('input', (e) => {
-    days = parseInt(e.target.value);
-    console.log(days);
-})
-irpfIn.addEventListener('input', (e) => {
-    irpf = (parseFloat(e.target.value)/100);
-    console.log(irpf);
-})
-formulario.addEventListener('submit', calculadora);
 
-resultClose.addEventListener("click", () =>{
-    resultAlert.style.display = "none";
+document.addEventListener('DOMContentLoaded', iniciarCalc)
 
-    // Cuando se cierran los resultados se resetea el formulario
-    resetForm();
-});
+function iniciarCalc() {
+
+    console.log("app iniciada");
+
+    // EVENT LISTENERS
+
+    salarioIn.addEventListener('input', (e) => {
+        salary = parseFloat(e.target.value);
+        console.log(salary);
+    })
+    diasIn.addEventListener('input', (e) => {
+        days = parseInt(e.target.value);
+        console.log(days);
+        console.log(e.target.value);
+    })
+    irpfIn.addEventListener('input', (e) => {
+        irpf = (parseFloat(e.target.value) / 100);
+        console.log(irpf);
+    })
+    formulario.addEventListener('submit', calculadora);
+    
+    resultClose.addEventListener("click", () => {
+        resultAlert.style.display = "none";
+    
+        // Cuando se cierran los resultados se resetea el formulario
+        resetForm();
+    });
 
 
 
 // FUNCIONES
-function calculadora(e){
+
+function calculadora(e) {
     e.preventDefault();
-    
+
 
     let salaryDaily = parseFloat(salary / days);
     let segsocCost;
     let segsocWorker;
 
-    try {
-        if( isNaN(salary) === false && isNaN(days) === false && isNaN(irpf) === false) {
+    if (isNaN(salary) === false && isNaN(days) === false && isNaN(irpf) === false) {
             // Cálculo COSTE SEG SOCIAL
-        switch (true){
-            case ( salaryDaily <= 275.00 ):
-                segsocCost = salaryDaily * days * 0.326;
-            break;
-            case (salaryDaily > 275.00 && salaryDaily <= 469.00):
-                segsocCost = 275 * days * 0.326;
-            break;
-            case (salaryDaily > 469.00 && salaryDaily <= 843.00): 
-                segsocCost = 347 * days * 0.326;
-            break;
-            case (salaryDaily > 843.00 && salaryDaily <= 1410.00): 
-                segsocCost = 414 * days * 0.326;
-            break;
-            default:
-                segsocCost = 551 * days * 0.326;
-        }
+            switch (true) {
+                case (salaryDaily <= 275.00):
+                    segsocCost = salaryDaily * days * 0.326;
+                    break;
+                case (salaryDaily > 275.00 && salaryDaily <= 469.00):
+                    segsocCost = 275 * days * 0.326;
+                    break;
+                case (salaryDaily > 469.00 && salaryDaily <= 843.00):
+                    segsocCost = 347 * days * 0.326;
+                    break;
+                case (salaryDaily > 843.00 && salaryDaily <= 1410.00):
+                    segsocCost = 414 * days * 0.326;
+                    break;
+                default:
+                    segsocCost = 551 * days * 0.326;
+            }
+
+            // Cálculo NETO
+            switch (true) {
+                case (salaryDaily <= 275.00):
+                    segsocWorker = salaryDaily * days * 0.064;
+                    break;
+                case (salaryDaily > 275.00 && salaryDaily <= 469.00):
+                    segsocWorker = 275 * days * 0.064;
+                    break;
+                case (salaryDaily > 469.00 && salaryDaily <= 843.00):
+                    segsocWorker = 347 * days * 0.064;
+                    break;
+                case (salaryDaily > 843.00 && salaryDaily <= 1410.00):
+                    segsocWorker = 414 * days * 0.064;
+                default:
+                    segsocWorker = 551 * days * 0.064;
+            }
+
+            // control de Bases mñaximas y mínimas
+            if (segsocCost > 1349.40) {
+                segsocCost = 1349.40;
+            }
+            if (segsocWorker > 264.92) {
+                segsocWorker = 264.92;
+            }
+            if (salaryDaily < 39) {
+                salaryDaily = 39;
+            }
+
+            // Calculos e imprimir resultado por pantalla
+            // 1. Mostrar valores introducidos
+            let inputSalaryShow = document.querySelector('.calculadora__inputs--salary');
+            inputSalaryShow.textContent = (new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(salary));
+            let inputDaysShow = document.querySelector('.calculadora__inputs--days');
+            inputDaysShow.textContent = `${days} días`
+            let inputIrpfShow = document.querySelector('.calculadora__inputs--irpf');
+            inputIrpfShow.textContent = `${irpf * 100}%`;
+
             
-        // Cálculo NETO
-        switch (true){
-            case ( salaryDaily <= 275.00 ):
-                segsocWorker = salaryDaily * days * 0.064;
-            break;
-            case (salaryDaily > 275.00 && salaryDaily <= 469.00):
-                segsocWorker = 275 * days * 0.064;
-            break;
-            case (salaryDaily > 469.00 && salaryDaily <= 843.00):
-                segsocWorker = 347 * days * 0.064;
-            break;
-            case (salaryDaily > 843.00 && salaryDaily <= 1410.00): 
-                segsocWorker = 414 * days * 0.064;
-            default:
-                segsocWorker = 551 * days * 0.064;
-        }
 
-        // control de Bases mñaximas y mínimas
-        if(segsocCost > 1349.40) {
-        segsocCost = 1349.40;
-        }
-        if(segsocWorker > 264.92) {
-            segsocWorker = 264.92;
-        }
-        if(salaryDaily < 39){
-            salaryDaily = 39;
-        }
+            // 2. Mostrar el coste total calculado
+            let totalCost = salary + segsocCost;
+            let costResult = document.querySelector('.calculadora__coste');
+            costResult.textContent = (new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(totalCost));
 
-        // Calculos e imprimir resultado por pantalla
-        // 1. Mostrar valores introducidos
-        let inputSalaryShow = document.querySelector('.calculadora__inputs--salary');
-        inputSalaryShow.textContent = (new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(salary));
-        let inputDaysShow = document.querySelector('.calculadora__inputs--days');
-        inputDaysShow.textContent = `${days} días`
-        let inputIrpfShow = document.querySelector('.calculadora__inputs--irpf');
-        inputIrpfShow.textContent = `${irpf * 100}%`;
+            // 3. Mostrar el neto calculado
+            parseFloat(irpfGross = salary * irpf);
+            let net = salary - segsocWorker - irpfGross;
+            let netResult = document.querySelector('.calculadora__neto');
+            netResult.textContent = (new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(net));
 
-        // 2. Mostrar el coste total calculado
-        let totalCost = salary + segsocCost;
-        let costResult = document.querySelector('.calculadora__coste');
-        costResult.textContent =  (new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(totalCost));
-        
-        // 3. Mostrar el neto calculado
-        parseFloat(irpfGross = salary * irpf);
-        let net = salary - segsocWorker - irpfGross;
-        let netResult = document.querySelector('.calculadora__neto');
-        netResult.textContent = (new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(net));
+            // 4. Mostrandose en pantalla
+            //4.1 Primero mostrar por pantalla spinner
 
-        // 4. Mostrandose en pantalla
-        //4.1 Primero mostrar por pantalla spinner
-
-        let spinnerBox = document.getElementById('spinnerBox');
-        spinnerBox.innerHTML = `
+            let spinnerBox = document.getElementById('spinnerBox');
+            spinnerBox.innerHTML = `
         <div class="spinner result-overlay-spinner">
             <div class="cube1"></div>
             <div class="cube2"></div>
         </div>
         `;
-        resultAlert.style.display = "block";
-        setTimeout(() => {
-            spinnerBox.remove();
 
-            //4.2 Añadir resultados después del spinner
-            resultAlert.classList.add('result-overlay');
-            resultAlert.style.display = "block";
-        }, 2000);
-         
+            setTimeout(() => {
+                spinnerBox.remove();
 
-        // Avisos de error en caso de dejar las celdas vacías
-        } else {
-           
+                //4.2 Añadir resultados después del spinner
+                resultAlert.classList.add('result-overlay');
+                resultAlert.style.display = "block";
+
+            }, 2000);
+
+            // Avisos de error en caso de dejar las celdas vacías
+    } else {
+
             let errorMessages = document.querySelectorAll('.form-inputs_item');
 
             errorMessages.forEach(errors => {
@@ -145,25 +156,22 @@ function calculadora(e){
 
                 errors.appendChild(errorBox);
                 setTimeout(() => {
-                errorBox.remove();
-                resetForm();
-             }, 3000);
-               
-            });      
+                    errorBox.remove();
+                    resetForm();
+                }, 3000);
+
+            });
         };
-        
-    } catch (error) {
-       
-    }
 
-    resetForm();
 
+    resultBtn.classList.add('btnDisabled');
+    resultBtn.setAttribute('disabled', '');
 };
 
 
-function resetForm () {
-    formulario.reset();
+function resetForm() {
+    location.reload();
+    iniciarCalc();
 }
-
-
-
+}
+;
